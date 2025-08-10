fa35d82ffbb8e9c0205cb5fc4f55299e2b081e2a
Maamoun Haj Najeeb <maamoun.haj.najeeb@gmail.com>
2025-08-10 17:52:48 +0300

    edits

--- dao.py
+++ dao.py
@@ -19,11 +19,11 @@ class Dao:
         return self.masterdb.applications.find_one({"shortname": name})
 
     def update_app_by_name(self, name: str, app: Dict[str, str]):
-        self.masterdb.applications.update({"shortname": name}, app)
+        self.masterdb.applications.update_one({"shortname": name}, app)
 
     def find_token(self, token):
         logging.info("find token: %s" % token)
         return self.db.tokens.find_one({"token": token})
 
     def add_token(self, token: Dict[str, str]):
-        return self.db.tokens.insert(token)
+        return self.db.tokens.insert_one(token)
